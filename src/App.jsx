24454cbc4cfcfa76e121d72d8c12580bfa886d3f
kingtimer12 <queentimer12@gmail.com>
2024-08-20 16:05:40 -0300

    chore: simple search update

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ function App() {
           {value: 2, label: "Banana"},
           {value: 3, label: "Avião"},
           {value: 4, label: "Bola"},
+          {value: 5, label: "Lula"},
+          {value: 6, label: "Faca"},
+          {value: 7, label: "Magnus"},
+          {value: 8, label: "Xadrez"},
         ]
         resolve(data);
       }, 1000);
@@ -20,9 +24,7 @@ function App() {
     <main className="flex flex-col justify-center items-center ">
       <form className="flex flex-col mt-2" onSubmit={handleSubmit((data) => console.log(data))}>
         <SearchSelect
-          className="px-4 py-2 min-w-[320px] h-[40px] border border-gray-300 bg-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 cursor-pointer focus:cursor-text"
           data={promiseOptions}
-          itemClassName="p-2 cursor-pointer hover:bg-gray-200"
           placeholder="Pesquisar" {...register("abc")}
         />
         <button type="submit" className="p-2 bg-blue-500 text-white mt-2">Submit</button>
